43c5a365c55b549dd3c12826add8f81ac5b53c53
Jeremy Neptune <jerenept@gmail.com>
2016-02-01 01:21:24 -0800

    made sock actually call shutdown properly made sock not block the socket from doing other useful things

--- sock.py
+++ sock.py
@@ -6,14 +6,18 @@
 import socket
 
 sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(("127.0.0.1", 8080))
+sock.connect(("raspberrypi.local", 8080))
 
 sock.send(b"KAPPA KEEPO")
 sock.close()
 while True:
   sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  sock.connect(("127.0.0.1", 8080))
-
-  msg = input(">")
+  try:
+    msg = input(">")
+  except KeyboardInterrupt:
+    sock.shutdown(socket.SHUT_RDWR)
+    sock.close()
+  sock.connect(("raspberrypi.local", 8080))
   sock.send(bytes(msg,'utf-8'))
+  sock.shutdown(socket.SHUT_RDWR)
   sock.close()
