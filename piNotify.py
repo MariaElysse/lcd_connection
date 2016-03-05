64e5a91edf72d0f0d05fc0975274a526a70e151b
Jeremy Neptune <jerenept@gmail.com>
2016-03-05 14:51:41 -0800

    made text not fall off the end of the screen

--- piNotify.py
+++ piNotify.py
@@ -7,10 +7,11 @@ import Adafruit_Nokia_LCD as LCD
 import Adafruit_GPIO.SPI as SPI
 from PIL import Image, ImageDraw, ImageFont
 import textwrap
-import APIKEY.API_KEY as APIKEY
+
 import time
 
 IMAGE_DIR = "icons/"
+APIKEY = "9c2639dae29c05b601b637b9ea2e544e"
 
 def weatherWriter(connexion):
     weather_dict = {}
@@ -36,7 +37,7 @@ def weatherWriter(connexion):
         except (KeyError):
             continue
         current_conditions.append("{0}C".format(str(weather_dict['currently']['temperature'])))
-        current_conditions.append("Rain: " + str(weather_dict['currently']['precipProbability']) + " chance")
+        current_conditions.append("Rain:" + str(weather_dict['currently']['precipProbability']) + " chance")
 
         con_icon = IMAGE_DIR + weather_dict['currently']['icon'] + ".png"
         icon = Image.open(con_icon)
@@ -46,7 +47,11 @@ def weatherWriter(connexion):
             draw = ImageDraw.Draw(image)
             draw.rectangle((0,0,LCD.LCDWIDTH, LCD.LCDHEIGHT), fill=255, outline=255)
             image.paste(icon, (0,0,LCD.LCDHEIGHT, LCD.LCDHEIGHT))
-            draw.text((48,0), item ,font=font)
+            itemWrapped = textwrap.wrap(item, 7)
+            y = 0
+            for line in itemWrapped:
+                draw.text((48,y), line ,font=font)
+                y+=7
             display.image(image)
             display.display()
             time.sleep(2)
@@ -54,14 +59,14 @@ def weatherWriter(connexion):
 def update():
     """this function should run forever"""
     start = arrow.now()
-    weather_con_request = requests.get("https://api.forecast.io/forecast/{}/34.0537,-118.2427?units=si".format(APIKEY))
+    weather_con_request = requests.get("https://api.forecast.io/forecast/9c2639dae29c05b601b637b9ea2e544e/34.0537,-118.2427?units=si")
     weather = weather_con_request.json()
     (receiver, sender) = Pipe()
     p = Process(target=weatherWriter, args=(receiver,))
     p.start()
     while True:
-        if (arrow.now() - start).seconds % 300 == 0:
-            weather_con_request = requests.get("https://api.forecast.io/forecast/{}/34.0537,-118.2427?units=si".format(APIKEY))
+        if (arrow.now() - start).seconds % 300 <5:
+            weather_con_request = requests.get("https://api.forecast.io/forecast/9c2639dae29c05b601b637b9ea2e544e/34.0537,-118.2427?units=si")
             weather = weather_con_request.json()
         sender.send(weather)
 
